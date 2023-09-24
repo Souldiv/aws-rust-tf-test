38fcbb2c234be5d1a6d343d10958a45e8d332c99
Souldiv <amrut546@gmail.com>
2023-09-24 17:26:08 -0400

    git clone - user data

--- tf_config/setup_files/init.sh
+++ tf_config/setup_files/init.sh
@@ -4,3 +4,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > Rustup.sh
 sh Rustup.sh -y
 cp -r ~/.{cargo,rustup,bash_profile,profile} /home/ubuntu/
 echo 'export PATH=$PATH:$HOME/.cargo/env' >> /etc/profile
+
+git clone https://github.com/Souldiv/aws-rust-tf-test
+cp -r ~/aws-rust-tf-test /home/ubuntu/
